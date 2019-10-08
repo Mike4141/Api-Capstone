bd1031d1f5c857765014ad20fed1df9c8be98a34
Michael Peavy <peavy20@gmail.com>
2019-10-07 21:09:31 -0500

    initial commit

---
+++ forecast.js
@@ -0,0 +1,28 @@
+
+const key = 'GCzHJ2aFcmGt3uITF6bhtA9dJ9xDGHeL ';
+
+//get weather info
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const  response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+
+//get city info
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+
